995fd49831f661b6e5c6db9c6ea859900e7eac9a
ArmandoDiazGit <78942285+ArmandoDiazGit@users.noreply.github.com>
2026-06-25 20:20:52 -0400

    cors error fix 4

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173,https://my-profile-v2.vercel.app/",
+    "http://localhost:5173,http://127.0.0.1:5173,https://my-profile-v2.vercel.app",
 ).split(",")
 
 REST_FRAMEWORK = {
